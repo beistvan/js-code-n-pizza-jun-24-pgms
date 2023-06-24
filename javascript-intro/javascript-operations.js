63206a2e751d4a72d8bf85e5fda66dc3bf95172b
Beregszászi István <beistvan@gmail.com>
2023-06-24 10:47:49 +0200

    JavaScript Operations

---
+++ javascript-intro/javascript-operations.js
@@ -0,0 +1,29 @@
+var counter = 0;
+
+counter = counter + 1;
+
+console.log(counter);
+
+var a = 5;
+
+var b = 3;
+
+var sum = a + b;
+
+console.log('Összeg: ' + sum);
+
+var difference = a - b;
+
+console.log('Különbség: ' + difference);
+
+var product = a * b;
+
+console.log('Szorzat: ' + product);
+
+var quotient = a / b;
+
+console.log('Hányados: ' + quotient);
+
+var remainder = a % b;
+
+console.log('Maradékos osztás: ' + remainder);
